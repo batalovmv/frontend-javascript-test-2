2e5247f8d1e108fd1be7d0573708186e2fdf63ec
batalovmv <batalov94@gmail.com>
2025-02-09 21:45:24 +0300

    поменял vite.config

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/',
+    base: '/frontend-javascript-test-2/',
   plugins: [react()],
     preview:{
         host:true,
