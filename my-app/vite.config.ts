36d3e6dfb78f32757abfbb3b1fbcd7b33da904f9
batalovmv <batalov94@gmail.com>
2025-02-09 17:52:48 +0300

    Настроил CI/CD для GitHub Pages (проект в my-app)

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/frontend-javascript-test-2/',
+    base: '/',
   plugins: [react()],
+    
 })
